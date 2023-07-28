69f03079cfc2907894ad9522e6a1d951a5cadadd
aminmk99 <amirzakuchaki@gmail.com>
2023-07-28 22:18:45 +0330

    Created constant colors folder

---
+++ lib/constants/my_colors.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+class SolidColors {
+  static const Color titleClr = Color.fromARGB(255, 40, 107, 184);
+  static const Color listTitleClr = Color.fromARGB(255, 64, 4, 87);
+}
+
+class GradientColors {}
