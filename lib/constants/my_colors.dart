e0986f8658823d8c5f418158a66b532bdf222120
aminmk99 <amirzakuchaki@gmail.com>
2023-07-30 10:38:44 +0330

    Added new constant colors

--- lib/constants/my_colors.dart
+++ lib/constants/my_colors.dart
@@ -1,9 +1,25 @@
-
 import 'package:flutter/material.dart';
 
 class SolidColors {
   static const Color titleClr = Color.fromARGB(255, 40, 107, 184);
   static const Color listTitleClr = Color.fromARGB(255, 64, 4, 87);
+  static const Color scaffoldBgClr = Color.fromARGB(255, 255, 255, 255);
+  static const Color lightTxt = Color.fromARGB(255, 255, 255, 255);
+  static const Color textTitleClr = Color.fromARGB(255, 0, 0, 0);
+  static const Color selectedPodcast = Color.fromARGB(255, 255, 139, 26);
+  static const Color submitArticle = Color.fromARGB(255, 209, 209, 209);
+  static const Color submitPodcast = Color.fromARGB(255, 246, 246, 246);
+  static const Color subTxt = Color.fromARGB(255, 197, 197, 197);
 }
 
-class GradientColors {}
+class GradientColors {
+  static const List<Color> bottomNav = [
+    const Color.fromARGB(255, 25, 0, 94),
+    const Color.fromARGB(255, 68, 4, 87)
+  ];
+
+  static const List<Color> tags = [
+    const Color.fromARGB(255, 0, 0, 0),
+    const Color.fromARGB(255, 63, 63, 63),
+  ];
+}
